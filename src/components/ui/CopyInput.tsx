e6cc2903bf13126cf226fb473b722dad9ceb1c61
Girish Vadlamudi <girishvadlamudi@outlook.com>
2025-05-27 18:49:18 +0200

    Typo in button import

--- src/components/ui/CopyInput.tsx
+++ src/components/ui/CopyInput.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { TextInput, Group } from "@mantine/core";
-import { Button } from "./Button";
+import { Button } from "@mantine/core";
 
 interface CopyInputProps {
   value: string;
